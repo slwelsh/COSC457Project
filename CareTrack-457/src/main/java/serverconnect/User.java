cfd3daaef2d1267873eef7172d8b532fa0ee1088
Sarah Welsh <96925684+slwelsh@users.noreply.github.com>
2024-05-03 19:47:27 -0400

    Update User.java

--- CareTrack-457/src/main/java/serverconnect/User.java
+++ CareTrack-457/src/main/java/serverconnect/User.java
@@ -1,10 +1,12 @@
 package serverconnect;
 
 public class User {
-  public String userID; 
+  public String userID;
+  public String userType; 
 
-  public User(String user){
+  public User(String user, String type){
     this.userID = user;
+    this.userType = type;
   }
 
 
@@ -15,4 +17,8 @@ public class User {
   public void setCurrentUser(String user){
     this.userID = user;
   }   
+
+  public String getCurrentUserType() {
+    return this.userType;
+  }
 }
